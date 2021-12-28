26b159d9d6540779db106d95cb1e321728a53386
tiiedye <tiiedye@gmail.com>
2021-12-28 09:12:58 -0800

    working on UI

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from tkinter import *
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -16,3 +17,15 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+image = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=image)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 24, "bold"))
+canvas.pack()
+
+# All code before main loop
+window.mainloop()
